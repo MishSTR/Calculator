d114255513fb49e4a45c486e3ab77a4c928374ea
Mikhail Raykhert <bus82can@gmail.com>
2025-07-31 21:32:00 -0400

    Calculatr done

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 "use strict"
+
+const equals = document.querySelector('#equals')
+const firstVar = document.querySelector('#firstVar')
+const secondVar = document.querySelector('#secondVar')
+const total = document.querySelector('#total')
+
+function push() {
+    const operator = document.querySelector('input[name="operation"]:checked')
+    if(operator) {            
+        switch (operator.value) {
+            case '+':
+                total.value = Number(firstVar.value) + Number(secondVar.value)
+                break;
+            case '-':
+                total.value = Number(firstVar.value) - Number(secondVar.value)
+                break;
+            case '*':
+                total.value = Number(firstVar.value) * Number(secondVar.value)
+                break;
+            case '/':
+                total.value = Number(firstVar.value) / Number(secondVar.value)
+                break;    
+            default:
+                break;
+        }
+    } else {
+        console.log('You did not take any opearation')
+        return
+    }
+}
+equals.addEventListener('click', push)
+
+
